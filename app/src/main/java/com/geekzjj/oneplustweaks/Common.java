97d9e2485b9ef9d31582b0da1ad3a67fa0fa3f74
GeekZJJ <geekzjj@gmail.com>
2020-04-18 12:07:14 +0800

    Change default preference value

--- app/src/main/java/com/geekzjj/oneplustweaks/Common.java
+++ app/src/main/java/com/geekzjj/oneplustweaks/Common.java
@@ -5,8 +5,10 @@ public class Common {
     public static String PACKAGE_ONEPLUS_DIALER = "com.android.dialer";
     public static String PACKAGE_ONEPLUS_MMS = "com.android.mms";
     public static String PACKAGE_ONEPLUS_DESKCLOCK = "com.oneplus.deskclock";
+    public static String PACKAGE_ONEPLUS_CONFIG = "com.oneplus.config";
     public static String PACKAGE_ANDROID = "android";
     public static String PACKAGE_SYSTEMUI = "com.android.systemui";
+    public static String PACKAGE_ANCROID_PHONE = "com.android.phone";
 
     public static final String ACTION_SETTINGS_CHANGED = "action_settings_changed";
 
@@ -21,14 +23,14 @@ public class Common {
     public static final String KEY_RECENTS_ADD_CLEAR_BTN = "recents_add_clear_button";
     public static final String KEY_CLEAR_RECENTS_LOCATION = "recents_clear_button_location";
 
-    public static final boolean DEFAULT_VOLUME_PANEL_EXPANDED = false;
+    public static final boolean DEFAULT_VOLUME_PANEL_EXPANDED = true;
     public static final String[] DEFAULT_VOLUME_EXPAND_STREAMS = {"3", "2", "4","0"};
     public static final int DEFAULT_VOLUME_PANEL_LOCATION = 1;
 
-    public static final int DEFAULT_HOTSPOT_2G_CHANNEL = 11;
+    public static final int DEFAULT_HOTSPOT_2G_CHANNEL = 0;
     public static final int DEFAULT_HOTSPOT_5G_CHANNEL = 44;
     public static final boolean DEFAULT_HOTSPOT_HIDDEN_SSID = false;
 
-    public static final boolean DEFAULT_RECENTS_ADD_CLEAR_BTN = false;
-    public static final int DEFAULT_CLEAR_RECENTS_LOCATION = 3;
+    public static final boolean DEFAULT_RECENTS_ADD_CLEAR_BTN = true;
+    public static final int DEFAULT_CLEAR_RECENTS_LOCATION = 5;
 }
